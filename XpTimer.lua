a9c694b348fcfdf2719e11c503898e0939c23235
Yuntao Cao <yuntao.cao@nocsys.com.cn>
2019-12-03 11:28:58 +0800

    remove target set

--- XpTimer.lua
+++ XpTimer.lua
@@ -101,13 +101,9 @@ function XpTimer:CreateMainWindow()
     local frame = AceGUI:Create("Frame2")
     frame:SetTitle("经验统计")
     frame:SetStatusText("停止")
-    --frame:SetCallback("OnClose", function(widget) AceGUI:Release(widget) end)
     frame:SetLayout("List")
     frame:SetWidth(42*SCALE_LENGTH)
-    frame:SetHeight(floor(35*SCALE_LENGTH))
-    --frame.frame:SetScript("OnDragStart",function(this)
-    --    message("hello")
-    --end )
+    frame:SetHeight(floor(32*SCALE_LENGTH))
     frame.frame:SetResizable(false)
     frame.frame["MoveFinished"] = XpTimer.OnDragStop
     frame.frame["OnHisButton"] = XpTimer.OnHisShow
@@ -126,13 +122,6 @@ function XpTimer:CreateMainWindow()
     XpTimer.btn_start:SetCallback("OnClick", XpTimer.OnStartBtn)
     ctl_btn_group:AddChild(XpTimer.btn_start)
 
-    --XpTimer.btn_pause = AceGUI:Create("Button")
-    --XpTimer.btn_pause:SetText("暂停")
-    --XpTimer.btn_pause:SetWidth(115)
-    --XpTimer.btn_pause:SetCallback("OnClick", XpTimer.OnPauseBtn)
-    --XpTimer.btn_pause:SetDisabled(true)
-    --ctl_btn_group:AddChild(XpTimer.btn_pause)
-
     XpTimer.btn_stop = AceGUI:Create("Button")
     XpTimer.btn_stop:SetText("结束")
     XpTimer.btn_stop:SetWidth(floor(17*SCALE_LENGTH))
@@ -140,39 +129,6 @@ function XpTimer:CreateMainWindow()
     XpTimer.btn_stop:SetDisabled(true)
     ctl_btn_group:AddChild(XpTimer.btn_stop)
 
-
-    XpTimer:insert_space(frame)
-
-    -- target set group
-    local target_set_group = AceGUI:Create("SimpleGroup")
-    target_set_group:SetLayout("Flow")
-    target_set_group:SetWidth(floor(38*SCALE_LENGTH))
-    frame:AddChild(target_set_group)
-    XpTimer.target_set_group = target_set_group
-
-    local tgt_set_label = AceGUI:Create("Label")
-    tgt_set_label:SetText("目标设置(经验/小时)")
-    tgt_set_label:SetWidth(20*SCALE_LENGTH)
-    self:ChangeFontSize(tgt_set_label,LABEL_SIZE)
-    tgt_set_label:SetPoint("CENTER")
-    target_set_group:AddChild(tgt_set_label)
-    XpTimer.tgt_set_label = tgt_set_label
-
-    --local tgt_edt_group = AceGUI:Create("SimpleGroup")
-    --tgt_edt_group:SetWidth(360)
-    --tgt_edt_group:SetLayout("Flow")
-    --target_set_group:AddChild(tgt_edt_group)
-
-    XpTimer.tgt_set_editbox = AceGUI:Create("EditBox")
-    XpTimer.tgt_set_editbox:SetWidth(16*SCALE_LENGTH)
-    XpTimer.tgt_set_editbox:SetCallback("OnEnterPressed", XpTimer.OnSetTargetBtn)
-    target_set_group:AddChild(XpTimer.tgt_set_editbox)
-
-    --local tgt_ensure_btn = AceGUI:Create("Button")
-    --tgt_ensure_btn:SetText("确定")
-    --tgt_ensure_btn:SetCallback("OnClick", XpTimer.OnSetTargetBtn)
-    --tgt_ensure_btn:SetWidth(80)
-    --tgt_edt_group:AddChild(tgt_ensure_btn)
 
 
     XpTimer:insert_space(frame)
@@ -184,16 +140,10 @@ function XpTimer:CreateMainWindow()
     frame:AddChild(target_time_group)
     XpTimer.target_time_group = target_time_group
 
-    -- information label
-    XpTimer.target_label = AceGUI:Create("Label")
-    XpTimer.target_label:SetText("目标:0")
-    XpTimer.target_label:SetWidth(18*SCALE_LENGTH)
-    self:ChangeFontSize(XpTimer.target_label,LABEL_SIZE)
-    target_time_group:AddChild(XpTimer.target_label)
 
     XpTimer.time_label = AceGUI:Create("Label")
-    XpTimer.time_label:SetText("时间:0分0秒")
-    XpTimer.time_label:SetWidth(18*SCALE_LENGTH)
+    XpTimer.time_label:SetText("累计时间:0分0秒")
+    XpTimer.time_label:SetWidth(36*SCALE_LENGTH)
     self:ChangeFontSize(XpTimer.time_label,LABEL_SIZE)
     target_time_group:AddChild(XpTimer.time_label)
 
@@ -226,38 +176,28 @@ function XpTimer:CreateMainWindow()
     XpTimer.speed_icon = AceGUI:Create("Icon")
     XpTimer.speed_icon:SetImage("Interface\\AddOns\\XpTimer\\textures\\gray")
     XpTimer.speed_icon.image:SetPoint("BOTTOMLEFT", 0, 0)
-    XpTimer.speed_icon:SetLabel("速度:0/小时(%0)")
+    XpTimer.speed_icon:SetLabel("速度:0/小时")
     XpTimer.speed_icon:SetImageSize(36*SCALE_LENGTH,3*SCALE_LENGTH)
     XpTimer.speed_icon:SetWidth(36*SCALE_LENGTH)
     XpTimer.speed_icon:SetHeight(24)
     self:ChangeFontSize(XpTimer.speed_icon,12)
     frame:AddChild(XpTimer.speed_icon)
 
-    --frame.closebutton:Hide()
-    --frame:Show()
+
     XpTimer.MainWindow = frame
 end
 
 function XpTimer:OnStartBtn()
 
-    if(XpTimer.target_exp == 0)then
-        message("请先设置目标经验速度")
-        return
-    end
 
     XpTimer.init_data()
     XpTimer.current_state = 2 -- 1:停止 2：运行中
     XpTimer.btn_start:SetDisabled(true)
     XpTimer.btn_stop:SetDisabled(false)
-    --C_Timer.After(3, XpTimer.TimerFeedback)
     XpTimer.MainWindow:SetStatusText("运行中")
 
 end
 
---function XpTimer:OnPauseBtn()
---    XpTimer.current_state = 2
---    XpTimer.btn_pause:SetText("继续")
---end
 
 function XpTimer:OnStopBtn()
     XpTimer.current_state = 1
@@ -268,27 +208,6 @@ function XpTimer:OnStopBtn()
     XpTimer:SaveCurrentData()
 end
 
-function XpTimer:OnSetTargetBtn()
-    local target = XpTimer.tgt_set_editbox:GetText()
-    XpTimer.target_exp = tonumber(target)
-    XpTimer.target_label:SetText(string.format("目标:%d",XpTimer.target_exp))
-end
-
-
---function XpTimer:TimerFeedback()
---
---    if(XpTimer.current_state == 1)then
---        return
---    end
---
---
---    XpTimer:Frame_update()
---
---
---    if(XpTimer.current_state == 2)then
---        C_Timer.After(3, XpTimer.TimerFeedback)
---    end
---end
 
 
 function XpTimer:OnUpdate()
@@ -326,7 +245,6 @@ function XpTimer:OnInitialize()
             self.db.profile.MainWindow.Position.y)
     XpTimer.MainWindow:Show()
     XpTimer.current_state = 1
-    XpTimer.target_exp = 0
     XpTimer.events:SetScript("OnUpdate",XpTimer.OnUpdate)
 end
 
@@ -428,7 +346,6 @@ function XpTimer:Frame_update()
     local time_to_up_min = floor(time_to_up_second/60)
 
     -- kill_num
-    --XpTimer.kill_num = XpTimer.kill_num + 1
     local averge_exp = 0
     local require_num = 0
     if(XpTimer.kill_num >= 1)then
@@ -438,37 +355,32 @@ function XpTimer:Frame_update()
 
 
     -- update frame
-    XpTimer.time_label:SetText(string.format("时间:%d分%d秒",ret_minutes,ret_seconds))
+    XpTimer.time_label:SetText(string.format("累计时间:%d分%d秒",ret_minutes,ret_seconds))
     XpTimer.accumulate_label:SetText(string.format("累计经验:%d 击杀数量:%d",XpTimer.all_exp,XpTimer.kill_num))
     XpTimer.update_level_label:SetText(string.format("升级时间:%d分 剩余经验:%d",time_to_up_min,(XpTimer.max_exp - current_exp)))
     XpTimer.averge_label:SetText(string.format("每怪经验:%d 剩余怪数:%d",averge_exp,require_num))
 
 
     local icon_img = ""
-    local per = floor(speed_exp_hour*100/XpTimer.target_exp)
 
-    if(per <= 50) then
+    if(speed_exp_hour <= 10000) then
         icon_img = "Interface\\AddOns\\XpTimer\\textures\\gray"
-    elseif(per >50 and per <= 60) then
+    elseif(speed_exp_hour >10000 and speed_exp_hour <= 17000) then
         icon_img = "Interface\\AddOns\\XpTimer\\textures\\green"
-    elseif(per >60 and per <= 75) then
+    elseif(speed_exp_hour >17000 and speed_exp_hour <= 24000) then
         icon_img = "Interface\\AddOns\\XpTimer\\textures\\yellow"
-    elseif(per >75 and per <= 90) then
+    elseif(speed_exp_hour >24000 and speed_exp_hour <= 30000) then
         icon_img = "Interface\\AddOns\\XpTimer\\textures\\blue"
-    elseif(per >90 and per <= 100) then
+    elseif(speed_exp_hour >30000 and speed_exp_hour <= 50000) then
         icon_img = "Interface\\AddOns\\XpTimer\\textures\\red"
-    elseif(per >100) then
+    elseif(speed_exp_hour >50000) then
         icon_img = "Interface\\AddOns\\XpTimer\\textures\\purple"
     end
 
     XpTimer.speed_icon:SetImage(icon_img)
-    local icon_len = 0
-    if(per >= 100)then
-        icon_len = 36*SCALE_LENGTH
-    else
-        icon_len = floor(36*SCALE_LENGTH*per/100)
-    end
-    XpTimer.speed_icon:SetLabel(string.format("速度:%d/小时(%d%%)",speed_exp_hour,per))
+    local icon_len = 36*SCALE_LENGTH
+
+    XpTimer.speed_icon:SetLabel(string.format("速度:%d/小时",speed_exp_hour,per))
     XpTimer.speed_icon:SetImageSize(icon_len,3*SCALE_LENGTH)
     XpTimer.speed_icon:SetWidth(36*SCALE_LENGTH)
     XpTimer.speed_icon:SetHeight(24)
@@ -501,16 +413,3 @@ function XpTimer:CHAT_MSG_COMBAT_XP_GAIN()
     XpTimer:Frame_update()
 end
 
-
---Sandbox = { }
---
---function Sandbox:HelloWorld()
---    message("Hello World!")
---end
---
---function Sandbox:HideGryphons()
---    MainMenuBarLeftEndCap:Hide()
---    MainMenuBarRightEndCap:Hide()
---end
---
---Sandbox:HelloWorld()
